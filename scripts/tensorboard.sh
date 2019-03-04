fc51aa9e3819f78409b65210f20c30acbb1ee21d
AliceSky <34699294+AliceSky@users.noreply.github.com>
2019-03-04 10:54:53 +0800

    Update tensorboard.sh

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 ]
+if [ $# -lt 2 ]  # $# 取得shell脚本参数个数，-lt  即 less than，小于
   then
     echo "insufficient arguments. case=$1, port=$2"
-    exit 0
+    exit 0 # exit 0 代表正常运行程序并退出程序,exit 1 代表非正常运行导致退出程序。其实目的就是: 程序退出后, 用户可以 echo $? 来查看是 0 还是 1, 从而达到检测程序是正常结束退出还是产生错误而退出的目的.
+
 fi
 
 case=$1
